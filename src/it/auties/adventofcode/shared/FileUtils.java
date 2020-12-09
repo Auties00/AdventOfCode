b91e36d0556bbef32d60278812bf24dabfa8425a
Auties00 <alautiero@gmail.com>
2020-12-09 12:10:23 +0100

    Cleaned code

---
+++ src/it/auties/adventofcode/shared/FileUtils.java
@@ -0,0 +1,25 @@
+package it.auties.adventofcode.shared;
+
+import it.auties.adventofcode.first.Main;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class FileUtils {
+  public static Optional<Stream<String>> readFile(String name) {
+    try {
+      var url = Main.class.getClassLoader().getResource(name);
+      if (url == null) {
+        return Optional.empty();
+      }
+
+      return Optional.of(Files.lines(Paths.get(url.toURI())));
+    } catch (IOException | URISyntaxException e) {
+      return Optional.empty();
+    }
+  }
+}
